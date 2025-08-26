2dc6e54b3094bc28be43954e4f6f8492ddc06489
rryando <rynaldo.dev@gmail.com>
2025-08-26 12:22:33 +0700

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -4,34 +4,16 @@ import { defineConfig } from "vite";
 // Change the import to use your runtime specific build
 import build from "@hono/vite-build/node";
 
-export default defineConfig(({ mode }) => {
-
-  if (mode === "client")
-    return {
-      esbuild: {
-        jsxImportSource: "hono/jsx/dom", // Optimized for hono/jsx/dom
-      },
-      build: {
-        rollupOptions: {
-          input: "./src/index.ts",
-          output: {
-            entryFileNames: "static/client.js",
-          },
-        },
-      },
-    };
-
-  return {
-    server: {
-      port: process.env.PORT ? parseInt(process.env.PORT) : 5173,
-    },
-    plugins: [
-      build({
-        entry: "src/index.ts",
-      }),
-      devServer({
-        entry: "src/index.ts",
-      }),
-    ],
-  };
+export default defineConfig({
+  server: {
+    port: process.env.PORT ? parseInt(process.env.PORT) : 5173,
+  },
+  plugins: [
+    build({
+      entry: "src/index.ts",
+    }),
+    devServer({
+      entry: "src/index.ts",
+    }),
+  ],
 });
